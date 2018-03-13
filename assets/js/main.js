e9316840f5a2614fc9613089c39c9870756f74e0
Jason Spiller <jasonspiller@Jasons-MBP.T-mobile.com>
2018-03-13 11:56:50 -0600

    Adding files through Unit 7

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/*
+
+// 
+flip over one card
+// fi
+
+*/
+
+var cardOne = "queen",
+  cardTwo = "king",
+  cardThree = "queen",
+  cardFour = "king";
+
+console.log(cardOne + cardTwo + cardThree + cardFour);
