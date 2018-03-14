cf7c01e6e43154ed8255a806e5dce94cc0788bd4
Jason Spiller <jason@thetargetlab.com>
2018-03-13 23:22:18 -0600

    Updating main.js to reflect Unit 9

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,21 +10,28 @@ purpose:  Logic for a Memory card matching game.
 var arrCards = ["queen", "king", "queen", "king"],
   arrCardsInPlay = [];
 
-var strCardOne = arrCards[0],
-  strCardTwo = arrCards[1];
-
-arrCardsInPlay.push(strCardOne);
-arrCardsInPlay.push(strCardTwo);
-
-console.log("User flipped over a " + arrCardsInPlay[0] + " and a " + arrCardsInPlay[1] + ".");
-
-
-if (arrCardsInPlay.length === 2) {
-
-  if (arrCardsInPlay[0] === arrCardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
-  }
+var checkForMatch = function () {
   
-}
+  if (arrCardsInPlay.length === 2) {
+
+    if (arrCardsInPlay[0] === arrCardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
+  }
+};
+
+var flipCard = function (cardId) {
+  
+  "use strict";
+
+  console.log("User flipped over a " + arrCards[cardId] + ".");
+  
+  arrCardsInPlay.push(arrCards[cardId]); 
+  
+  checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
